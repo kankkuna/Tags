2b44b8ccc90ccd4f2d0933a6157cb562224e6e19
kankkuna <katherine.lef@hotmail.com>
2017-11-10 18:03:58 -0500

    se inicia boucles

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 //function show(){
   var tabs = document.querySelectorAll(".tabs");
+  var parrafos = document.querySelectorAll(".parrafos");
   /*var tabs2 = document.getElementById("tab2");
   tab2.addEventListener("click",showContainer2);
 
@@ -11,6 +12,7 @@ function showContainer2 (){
   container1.classList.add("hidden");
 }*/
 
+
 function toggleTab (elem){
   for (var i = 0; i <elem.length; i++) {
     elem[i].addEventListener("click",showContainers);
@@ -18,8 +20,17 @@ function toggleTab (elem){
 }
 
 function showContainers(){
-this.classList.add("red");
+  for(var j=0; j<parrafos.length; j++){
+    if(this.dataset.text === parrafos[j].dataset.text){
+      if (parrafos[j].classList.contains("hidden")){
+        parrafos[j].classList.remove("hidden");
+      }
+    } else{
+    parrafos[j].classList.add("hidden");
+   }
   }
+//this.classList.add("red");
+}
 window.onload = function() {
   toggleTab(tabs);
 }
